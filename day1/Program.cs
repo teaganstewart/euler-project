3098a16bf6a0c9735fc247b6b27e8ce50a09b7ef
teagachu <teagachu@users.noreply.github.com>
2020-11-20 16:11:41 +1300

    Start

---
+++ day1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EulerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
