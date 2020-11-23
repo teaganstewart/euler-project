fcadd82e7b17592876a2085a0df6ca488093ed49
teaganstewart <teaganstewart@users.noreply.github.com>
2020-11-23 23:47:54 +1300

    Started Palindromes Algorithm
    Base Algorithm written - isn't fully functional yet.

---
+++ day4/Palindromes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+        }
+    }
+      
+}
